ac4c0c9b607d241ad7b5440456acae85c2d75304
Luke Slater <tinmachin3@gmail.com>
2020-01-22 14:33:14 +0000

    add transparent parsing of PDF documents [close #39]

---
+++ klib/PDFReader.groovy
@@ -0,0 +1,46 @@
+package klib
+
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.text.PDFTextStripper
+
+public class PDFReader {
+  def pages = []
+
+  def PDFReader(file) {
+    def reader
+    try {
+      reader = PDDocument.load(file)
+      def stripper = new PDFTextStripper()
+
+      (1..reader.getNumberOfPages()).each {
+        stripper.setStartPage(it)
+        stripper.setEndPage(it)
+
+        def text = stripper.getText(reader).toLowerCase()
+
+        text = text.replaceAll('\u2022', '.  ')
+        text = text.replaceAll('â€“', '. ')
+        text = text.replaceAll('-', '.  ')
+        text = text.replaceAll('– ', '. ')
+        text = text.replaceAll('\\s+', ' ')
+        text = text.replaceAll(', \\?', '. ?')
+        text = text.replaceAll('\\.', '. ')
+
+        pages << text
+      }
+
+      reader.close()
+    } catch(e) {
+      println "Failed to load document!"
+      e.printStackTrace() 
+    } 
+  } 
+
+  Iterator iterator() {
+    return pages.iterator()
+  }
+
+  def getText() {
+    this.collect().join('\n')
+  }
+}
